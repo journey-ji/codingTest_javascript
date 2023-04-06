1fb4a3267e990acc232164002f77bba1072ab149
journey-ji <journeyji95@gmail.com>
2023-04-06 22:40:42 +0900

    '파이썬_알고리즘_인터뷰'

---
+++ python/알고리즘인터뷰/문자열조작/그룹애너그램.py
@@ -0,0 +1,11 @@
+import collections
+def groupAnagrams( strs):
+    anagrams = collections.defaultdict(list)
+
+    for word in strs:
+        # 정렬하여 딕셔너리에 추가
+        anagrams[''.join(sorted(word))].append(word)
+    return list(anagrams.values())
+
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
---
+++ python/알고리즘인터뷰/문자열조작/유효한팰린드롬.py
@@ -0,0 +1,27 @@
+def solution(s):
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+    while len(strs) >1:
+        if(strs.pop(0) !=strs.pop()):
+            return False
+    return True
+print(solution("A man, a plan, a canal: Panama"))
+
+# deque 이용시 속도향상가능
+
+from collections import deque
+
+def solution_by_deque(s):
+    strs: Deque = deque()
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs)>1:
+        if strs.popleft() != strs.pop():
+            return False
+    return True
+
+print(solution_by_deque('A man, a plan, a canal: Panama'))

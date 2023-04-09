04deb56ce9e19bd17dcc5ac44ea42a423350b86a
journey-ji <journeyji95@gmail.com>
2023-04-09 22:45:41 +0900

    파이썬 알고리즘 인터뷰 - 빗물트래핑 by stack

--- python/알고리즘인터뷰/배열/빗물 트래핑.py
+++ python/알고리즘인터뷰/배열/빗물 트래핑.py
@@ -18,7 +18,26 @@ def trap(height):
         
 
     return volume
-print(trap([0,1,0,2,1,0,1,3,2,1,3,1]))
+
+
+def trapByStack(height):
+    stack = []
+    volume = 0
+    for i in range(len(height)):
+        while stack and height[i] > height[stack[-1]]:
+            top = stack.pop()
+            if not len(stack):
+                break
+
+            dist = i - stack[-1] -1
+            waters = min(height[i],height[stack[-1]]) - height[top]
+
+            volume += dist * waters
+        stack.append(i)
+    return volume
+
+
+print(trapByStack([0,1,0,2,1,0,1,3,2,1,2,1,2]))
 
 
 
